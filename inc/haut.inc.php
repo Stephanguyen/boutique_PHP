89eb89e4b97cb216dbbfcd3b0aff54dfeb51dce2
Thomasg75 <srtp.35.000@gmail.com>
2018-05-15 15:40:17 +0200

    haut version 2

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -2,42 +2,27 @@
 <html>
 	<head>
 		<meta charset='utf-8'>
-		<link rel="stylesheet" href="<?php echo URL; ?>css/style.css">
-		<link rel="stylesheet" href="<?php echo URL; ?>css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo URL; ?>inc/css/style.css">
+		<link rel="stylesheet" href="<?php echo URL; ?>inc/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 		<title>Boutique</title>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-		<title>Boutique</title>
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 	</head>
-	<body>
+	<body> 
 		<header>
 			<div class="container">
 				<div>
 					<a href="" title="Mon Site">MonSite.com</a>
 				</div>
-			
-			</div>
-		</header>
-			<section>
-				<div class="container">
-            <div class="starter-template">
-
-            </div>
-
-
-
-
-
-            <nav>
+				<nav>
 					<ul>
 						<li><a href="<?= URL; ?>index.php">Accueil</a></li>
 					<?php if(internauteEstConnecteEtEstAdmin()): ?>
 						<li><a href="<?= URL; ?>admin/gestion-des-produits.php">Gestion des produits</a></li>
 						<li><a href="<?= URL; ?>admin/gestion_commandes.php">Gestion des commandes</a></li>
-						<li><a href="<?= URL; ?>admin/gestion_membres.php">Gestion des membres</a></li>
 					<?php endif; ?>
 					<?php if(internauteEstConnecte()): ?>
 						<li><a href="<?= URL; ?>panier.php">Panier</a></li>
@@ -70,3 +55,7 @@
 					<?php endif; ?>
 					</ul>
 				</nav>
+			</div>
+		</header>
+			<section>
+				<div class="container">
